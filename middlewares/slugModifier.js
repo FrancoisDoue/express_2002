3aa3ef86b4176ff8f0b00422a0275d9f0eb0261f
FrancoisDoue <doue.francois@outlook.com>
2024-02-02 10:30:05 +0100

    regex added to slugModifier middleware

--- middlewares/slugModifier.js
+++ middlewares/slugModifier.js
@@ -1,5 +1,5 @@
 export default (req, _res, next) => {
-    if(!req.body?.title) return next()
+    if(!req.body?.title || req.body?.slug) return next()
     req.body.slug = req.body.title.toLowerCase().replaceAll(/[ '"()-\.]/gi, '_')
     next()
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import movieController from '../controller/movieController.js';
-import slugManager from '../../middlewares/slugManager.js';
+import slugModifier from '../../middlewares/slugModifier.js';
 
 const router = express.Router()
 
 router.get('/', movieController.getAllMovies)
 router.get('/:title', movieController.getMovieByTitle)
-router.put('/update/:title', slugManager, movieController.updateMovie)
+router.put('/update/:title', slugModifier, movieController.updateMovie)
 router.post('/', movieController.addNewMovie)
 router.delete('/delete/:title', movieController.deleteMovie)
 // router.use('/')
